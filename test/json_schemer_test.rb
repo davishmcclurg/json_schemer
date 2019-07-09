d53a39892ac9ac622f7c640f75d61ea09984f297
Sebastian Spiess <sebastian.spiess@invision.de>
2019-07-09 15:46:31 +0200

    Add test for discriminator and .prettierrc

--- test/json_schemer_test.rb
+++ test/json_schemer_test.rb
@@ -536,12 +536,14 @@ class JSONSchemerTest < Minitest::Test
     'openapi3' => JSONSchemer::Schema::OpenApi3
   }.each do |version, draft_class|
 
+    paths = []
+
     if version == 'openapi3'
       version = 'draft4'
-      paths = Dir['test/openapi3/**/*.json']
+      paths += Dir['test/openapi3/**/*.json']
     end
 
-    paths = Dir['JSON-Schema-Test-Suite/tests/#{version}/**/*.json']
+    paths += Dir['JSON-Schema-Test-Suite/tests/#{version}/**/*.json']
 
     paths.each_with_index do |file, file_index|
       JSON.parse(File.read(file)).each_with_index do |defn, defn_index|
