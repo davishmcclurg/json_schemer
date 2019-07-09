d194fa0872c9ae106b4767918825200cb272a59b
Andreas Haller <andreas.haller@invision.de>
2019-07-09 17:50:24 +0200

    WIP: Support discriminator

--- lib/json_schemer/schema/openapi3.rb
+++ lib/json_schemer/schema/openapi3.rb
@@ -7,6 +7,21 @@ module JSONSchemer
         return if instance.schema['nullable'] && instance.data.nil?
         super
       end
+
+      def discriminate(many_of, discriminator, data)
+        return many_of unless many_of && discriminator
+
+        property_name = discriminator['propertyName']
+        schema_name = data[property_name]
+        filtered = many_of.filter do |item|
+          resolve_schema_name(item) == schema_name
+        end
+        filtered
+      end
+
+      def resolve_schema_name(item)
+        File.basename(item['$ref'])
+      end
     end
   end
 end
