ed9963b35be9d81b2f3b2f38afaaa6b14dff4d3a
Sebastian Spiess <sebastian.spiess@invision.de>
2019-07-10 16:16:59 +0200

    Add check for missing discriminator property

--- lib/json_schemer/schema/openapi3.rb
+++ lib/json_schemer/schema/openapi3.rb
@@ -11,17 +11,13 @@ module JSONSchemer
       def discriminate(many_of, discriminator, data)
         return many_of unless many_of && discriminator
 
-        property_name = discriminator['propertyName']
-        property_value = data[property_name]
+        property_value = data[discriminator['propertyName']]
+        return [] if property_value.nil?
         schema_name = discriminator['mapping'][property_value] || property_value
 
         many_of.select do |item|
-          resolve_schema_name(item) == File.basename(schema_name)
+          File.basename(item['$ref']) == File.basename(schema_name)
         end
-      end
-
-      def resolve_schema_name(item)
-        File.basename(item['$ref'])
       end
     end
   end
