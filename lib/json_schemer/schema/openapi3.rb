5e5c1bdf39d2b038de6e89891114bd2745518962
Sebastian Spiess <sebastian.spiess@invision.de>
2019-08-02 09:04:23 +0200

    Replace dig since this is unsupported in ruby 2.2

--- lib/json_schemer/schema/openapi3.rb
+++ lib/json_schemer/schema/openapi3.rb
@@ -5,6 +5,7 @@ module JSONSchemer
     class OpenApi3 < Draft7
       def validate_type(instance, type, &block)
         return if instance.schema['nullable'] && instance.data.nil?
+
         super
       end
 
@@ -14,7 +15,9 @@ module JSONSchemer
         property_value = data[discriminator['propertyName']]
         return [] if property_value.nil?
 
-        schema_name = discriminator.dig('mapping', property_value) || property_value
+        schema_name = discriminator['mapping'] &&
+                      discriminator['mapping'][property_value] ||
+                      property_value
 
         many_of.select do |item|
           File.basename(item['$ref']) == File.basename(schema_name)
