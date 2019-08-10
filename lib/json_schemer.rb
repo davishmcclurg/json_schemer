cd02b7688ef46a45fd32a7681a41f69937da2cdd
David Harsha <davishmcclurg@gmail.com>
2019-08-10 12:34:04 -0700

    Require `regexp_parser`
    This was left out accidentally.
    
    `Regexp::Scanner` is used in lib/json_schemer/schema/base.rb to parse
    ECMA 262 regular expressions.
    
    Related: https://github.com/davishmcclurg/json_schemer/pull/34

--- lib/json_schemer.rb
+++ lib/json_schemer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'base64'
+require 'ipaddr'
 require 'json'
 require 'net/http'
 require 'pathname'
@@ -8,7 +9,7 @@ require 'uri'
 
 require 'ecma-re-validator'
 require 'hana'
-require 'ipaddr'
+require 'regexp_parser'
 require 'uri_template'
 
 require 'json_schemer/version'
