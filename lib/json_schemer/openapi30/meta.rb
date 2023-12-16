48a7c0bf6f66afe8f84129674063cd57316d4f68
David Harsha <davishmcclurg@gmail.com>
2023-12-16 14:18:49 -0800

    Use `BINARY` instead of `ASCII_8BIT`
    `BINARY` is an alias of `ASCII_8BIT` and I think the name makes things
    more clear (particularly for the `binary` format).
    
    See: https://bugs.ruby-lang.org/issues/18576

--- lib/json_schemer/openapi30/meta.rb
+++ lib/json_schemer/openapi30/meta.rb
@@ -5,7 +5,7 @@ module JSONSchemer
     # https://spec.openapis.org/oas/v3.0.3#data-types
     FORMATS = OpenAPI31::FORMATS.merge(
       'byte' => proc { |instance, _value| ContentEncoding::BASE64.call(instance).first },
-      'binary' => proc { |instance, _value| instance.is_a?(String) && instance.encoding == Encoding::ASCII_8BIT },
+      'binary' => proc { |instance, _value| instance.is_a?(String) && instance.encoding == Encoding::BINARY },
       'date' => Format::DATE
     )
     SCHEMA = {
